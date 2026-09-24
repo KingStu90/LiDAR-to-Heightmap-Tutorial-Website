f9ca00a46005026a7cb1ff0215f66dfc8e97f724
KingStu90 <306324780+KingStu90@users.noreply.github.com>
2026-09-24 05:45:26 -0700

    Fix Potree hierarchy range request

--- public/potree/build/potree/potree.js
+++ public/potree/build/potree/potree.js
@@ -66554,7 +66554,6 @@ void main() {
 
 			let response = await fetch(hierarchyPath, {
 				headers: {
-					'content-type': 'multipart/byteranges',
 					'Range': `bytes=${first}-${last}`,
 				},
 			});
